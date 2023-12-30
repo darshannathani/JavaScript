2b6e0ad44b56af7dc2fc610975fa5f1abb2d041c
Darshan Nathani <dattunathani@gmail.com>
2023-12-31 00:34:33 +0530

    leak solved

--- Practice/variables.js
+++ Practice/variables.js
@@ -2,8 +2,8 @@ const uid = "abc123";
 
 var fullName = "Darshan Nathani";
 var email = "21it472@bvmengineering.ac.in";
-var password = "goldenhour@D26";
-var confirmPassword = "goldenhour";
+var password = "funny that you didnt get the password";
+var confirmPassword = "funny that you didnt get the password";
 var courseCount = 0;
 var isLoggedInFromGoogle= false;
 
